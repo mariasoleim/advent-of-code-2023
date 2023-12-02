c4fc148032abee0ff80bd4e751bf4a6799191afa
Maria Soleim <maria.soleim@finn.no>
2023-12-03 00:51:36 +0100

    Day 2 task 2

--- src/main/kotlin/day2/Main.kt
+++ src/main/kotlin/day2/Main.kt
@@ -8,12 +8,12 @@ fun main() {
 
     val result = task1("./src/main/kotlin/day2/input.txt")
     println("Task 1 result: $result")
-//
-//    val testResult2 = task2("./src/main/kotlin/day2/input-task2-test.txt")
-//    println("Task 2 test result: $testResult2")
-//
-//    val result2 = task2("./src/main/kotlin/day2/input.txt")
-//    println("Task 2 result: $result2")
+
+    val testResult2 = task2("./src/main/kotlin/day2/input-test.txt")
+    println("Task 2 test result: $testResult2")
+
+    val result2 = task2("./src/main/kotlin/day2/input.txt")
+    println("Task 2 result: $result2")
 }
 
 fun task1(filePath: String): Int {
@@ -27,13 +27,27 @@ fun task1(filePath: String): Int {
 }
 
 fun task2(filePath: String): Int {
-    return 0
+    val games = readFileAsLinesUsingUseLines(filePath).map { Game.fromFile(it) }
+    return games.sumOf { it.getPower() }
 }
 
 data class Game(val id: Int, val subsets: List<Subset>) {
 
     fun isPossible(maximumNumberOfCubes: Map<Color, Int>): Boolean {
         return subsets.all { it.isPossible(maximumNumberOfCubes) }
+    }
+
+    private fun minimumNumberOfCubes(): Map<Color, Int> {
+        return mapOf(
+            Color.RED to subsets.maxOf { it.getNumberOfColor(Color.RED) },
+            Color.GREEN to subsets.maxOf { it.getNumberOfColor(Color.GREEN) },
+            Color.BLUE to subsets.maxOf { it.getNumberOfColor(Color.BLUE) }
+        )
+    }
+
+    fun getPower(): Int {
+        val minimumNumberOfCubes = minimumNumberOfCubes()
+        return minimumNumberOfCubes[Color.RED]!! * minimumNumberOfCubes[Color.GREEN]!! * minimumNumberOfCubes[Color.BLUE]!!
     }
 
     companion object {
@@ -52,6 +66,10 @@ data class Subset(val subsetColors: List<SubsetColor>) {
 
     fun isPossible(maximumNumberOfCubes: Map<Color, Int>): Boolean {
         return subsetColors.all { it.isPossible(maximumNumberOfCubes) }
+    }
+
+    fun getNumberOfColor(color: Color): Int {
+        return subsetColors.find { it.color == color }?.numberOfCubes ?: 0
     }
 
     companion object {
