6130b023e23514c57842438bc96c57298dfe6ff7
Maria Soleim <maria.soleim@finn.no>
2023-12-03 22:20:57 +0100

    Day 3 task 1

---
+++ src/main/kotlin/day3/Main.kt
@@ -0,0 +1,84 @@
+package day3
+
+import day1.readFileAsLinesUsingUseLines
+import java.lang.IndexOutOfBoundsException
+
+
+class Day3(filePath: String) {
+
+    private val rows: List<String> = readFileAsLinesUsingUseLines(filePath)
+
+    fun task1(): Int {
+        return getSymbolLocations()
+            .flatMap { getAdjacentCoordinatesWithDigits(it) }
+            .map { getCoordinatesForFirstDigitInNumberAtCoordinate(it) }.toSet()
+            .sumOf { getNumberBeginningAtCoordinate(it) }
+    }
+
+    fun task2(): Int {
+        return 0
+    }
+
+    private fun getSymbolLocations(): List<Pair<Int, Int>> {
+        val result = mutableListOf<Pair<Int, Int>>()
+        rows.forEachIndexed { indexRow, row -> run {
+            row.forEachIndexed { indexColumn, column -> run {
+                if (!column.isDigit() && column != '.') {
+                    result.add(Pair(indexRow, indexColumn))
+                }
+            } }
+        } }
+        return result
+    }
+
+    private fun getAdjacentCoordinatesWithDigits(symbol: Pair<Int, Int>): List<Pair<Int, Int>> {
+        val i = symbol.first
+        val j = symbol.second
+
+        val adjacentPairs = listOf(
+            Pair(i-1,j-1),
+            Pair(i-1, j),
+            Pair(i-1, j+1),
+            Pair(i, j-1),
+            Pair(i, j+1),
+            Pair(i+1, j-1),
+            Pair(i+1, j),
+            Pair(i+1, j+1),
+        )
+
+        return adjacentPairs.filter {
+            try {
+                rows[it.first][it.second].isDigit()
+            } catch (e: IndexOutOfBoundsException) {
+                false
+            }
+        }
+    }
+
+    private fun getCoordinatesForFirstDigitInNumberAtCoordinate(pair: Pair<Int, Int>): Pair<Int, Int> {
+        var col = pair.second
+        while (col >= 0 && rows[pair.first][col].isDigit()) {
+            col -= 1
+        }
+        return Pair(pair.first, col + 1)
+    }
+
+    private fun getNumberBeginningAtCoordinate(pair: Pair<Int, Int>): Int {
+        val row = rows[pair.first]
+        val rowFromFirstDigit = row.substring(pair.second)
+        return rowFromFirstDigit.takeWhile { it.isDigit() }.toInt()
+    }
+}
+fun main() {
+    val testResult = Day3("./src/main/kotlin/day3/input-test.txt").task1()
+    println("Task 1 test result: $testResult")
+
+    val result = Day3("./src/main/kotlin/day3/input.txt").task1()
+    println("Task 1 result: $result")
+
+//    val testResult2 = task2("./src/main/kotlin/day2/input-test.txt")
+//    println("Task 2 test result: $testResult2")
+//
+//    val result2 = task2("./src/main/kotlin/day2/input.txt")
+//    println("Task 2 result: $result2")
+}
